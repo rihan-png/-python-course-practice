9e8525534abca3c36ffa6442af5449acada4a856
RIHAN MAHAMADASIF BAGWAN <rihanbagwan74@gmail.com>
2026-01-14 15:54:18 +0530

    i understand what i can do with list methods

---
+++ list_methodft.wwe.py
@@ -0,0 +1,11 @@
+candidates = ["John cena","undertaker","Roman Reings",5,4.45,True]
+prints(candidates)
+candidates.append("The Rock")
+l1=[1,23,54,67,32,12]
+Ranks=[1,23,54,67,32,12]
+#l1.sort()
+#l1.reverse()
+#l1.insert(2,22222)
+disqualified= Ranks.pop(3)
+print(disqualified)
+print(Ranks)
