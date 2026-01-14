38db8fda7b4f24a6bf6a1e1704100475c1087488
RIHAN MAHAMADASIF BAGWAN <rihanbagwan74@gmail.com>
2026-01-14 15:33:31 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,6 @@
+purchase list = ["Apples","Orange",5,43.5,False,"Aakash","ROhan"]
+
+prints(friends[0])
+friends[0]="Grapes" #unlike list strings are mutable 
+print(friends[0])
+print(friends[1:4])
